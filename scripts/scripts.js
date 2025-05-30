a1d5080e5e1dc79d8519bb6d9695711aabc3fcc7
abeckwith <anthonyrbeckwith@gmail.com>
2025-05-30 11:19:53 -0400

    alter internship description

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -778,14 +778,14 @@ function formatLocation(loc) {
 function setMenu(whichOneGray) {
     menu =
         '<a id="0" class="top-links" href="index.html">Home</a>' +
-        '<a id="1" class="search-buttons" href="bios.html">Name Database</a>' +
-        '<a id="2" class="search-buttons" href="topical.html">Subject Database</a>' +
+        '<a id="1" class="search-buttons" href="bios.html">Name Index</a>' +
+        '<a id="2" class="search-buttons" href="topical.html">Subject Index</a>' +
         '<a id="3" class="top-links" href="search.html">Site Search &#x1F50E;</a>' +
         '<a id="4" class="top-links" href="recents.html">Recent Entries</a>' +
         '<a id="5" class="top-links" href="links.html">Links</a>' +
         '<a id="6" class="top-links" href="sources.html">Sources</a>' +
         '<a id="7" class="top-links" href="nominations.html">Nominations</a>' +
-        '<a id="8" class="top-links" href="internship.html">Internships</a>' +
+        '<a id="8" class="top-links" href="internship.html">Get Involved</a>' +
         '<a id="9" class="top-links" href="about.html">About</a>' +
         '<a href="javascript:void(0);" class="icon" onclick="resizeMenu()">' +
         '<i class="fa fa-bars"> <span id="menu-text">MENU</span></i>' +
