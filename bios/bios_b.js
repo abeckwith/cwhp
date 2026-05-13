2f65a91a288e190b5a2dc1e29d744bf408aa9288
abeckwith <anthonyrbeckwith@gmail.com>
2026-05-13 06:44:15 -0400

    Blackburn and nicgorski

--- bios/bios_b.js
+++ bios/bios_b.js
@@ -3339,6 +3339,125 @@ b_bios = [
         authors: "Anthony Beckwith",
         categories: ["Telephone company worker", "Library Trustee"],
     },
+      {
+        poc: false,
+        organization: false,
+        lastName: "Blackburn",
+        firstName: "Chris",
+        middleName: "",
+        familyName: "",
+
+        title: `Human Rights Activist`,
+        birthDate: "00.00.1948",
+        birthLocation: "Chicago, Illinois",
+        deathDate: "",
+        deathLocation: "",
+        narrative: `
+
+      Chicago-born Chris Blackburn attended seminary in Newton, Massachusetts, and later moved to Wisconsin to work as a 
+      campus minister.  She returned to the Boston area in 1977 and worked as the executive director of The University 
+      Christian Movement in New England, which had an office in the basement of First Church in Cambridge-Congregational,
+       from 1978 to 1994. 
+        <br><br>
+
+        In 1966, Darlene "Dar" Nicgorski became a nun in the School Sisters of St. Francis order. As part of her work, 
+        she started a childcare center in Holly Springs, Mississippi. 
+        <br><br>
+
+        In Nicgorski's obituary, it was reported that as she was preparing to head to Guatemala for missionary work, 
+        she 
+        <blockquote>
+        "...heard the shocking news that four Catholic women missionaries from the United States, working in 
+        El Salvador, were raped and murdered. Undeterred, she still undertook the journey. In July 1981, a few months 
+        after her arrival in Guatemala, the village priest and his driver were assassinated by gunmen from the Guatemalan military. 
+        A villager warned Ms. Nicgorski and the other nuns that their names were on a list and their lives were also in danger. 
+        Ms. Nicgorski and the others made the treacherous journey out of Guatemala. While waiting for her future assignment, 
+        she continued to serve the refugees and the poor in the camps that sprung up in Honduras and on the Mexican border."
+        </blockquote>
+
+        Nicgorski was one of 15 church workers that were tried by the federal government in 1985 in Arizona as part of the 
+        infamous "Sanctuary Trials." The US Government, under Ronald Reagan's Justice Department inducted, tried, and
+         convicted many church workers, nuns, and ministers for helping Central American refugees who were fleeing violence in their own 
+         countries. Nicgorski was found guilty of conspiracy to violate United States immigration 
+         laws and was sentenced to five years of probation. Despite her sentence, Nicgorski continued to advocate for these issues, speaking
+          at churches, schools, and public events in over 200 cities.
+        <br><br>
+
+        After the trial, Nicgorski moved to Massachusetts, where she met Chris Blackburn in the Rev. Dr. Carter Heyward's 
+        Feminist Liberation Theology class at the Episcopal Divinity School in Cambridge. The couple would be together 
+        (marrying in 2008) for 29 years. They bought a home together on Harvey Street in North Cambridge in January, 1991, 
+        and were highly involved in local political and social issues, as well as staying engaged in work on behalf of 
+        refugees. The couple also worked to support local women candidates for City Council and other offices.
+        <br><br>
+
+        In 1995, the Cambridge Commission on the Status of Women (also known as The Cambridge Women's Commission) worked with the Cambridge Arts Commission, the Cambridge Historical 
+        Commission, and City Councilor Katherine Triantafillou, along with local residents Blackburn, Nicgorski, and Marcia 
+        Diehl to create the Cambridge Women's Trail. The trail was designed to celebrate the little-recognized accomplishments of Cambridge
+         women and their contributions to their community over the history of the city. Blackburn recalls: 
+        <blockquote>
+
+        "My office for the University Christian Movement in New England was in the church at 11 Garden Street in Cambridge, 
+        so I walked to Harvard Square, almost daily, where I'd see statues of males in the Cambridge Common. All these men 
+        were mostly being honored for their participation in some war. I'd go home and tell Dar that all these men are being 
+        honored and remembered in Cambridge but no women were honored in any visible way. So, out of that frustration and 
+        awareness, Dar and I decided we'd organize something to honor a woman, in Cambridge." 
+        </blockquote>
+        This effort led to the creation of 
+        the May Sarton memorial (Sarton was a well-known Cambridge poet, novelist, and writer) near the Cambridge Public Library, was
+         the first step towards the larger
+        Cambridge Women's Trail project and, eventually, the establishment of the Cambridge Women's Heritage Project.
+        <br><br>
+
+        In 1996, the Cambridge Peace Commission's "Peace and Justice Award" was jointly given to Nicgorski and Blackburn. In the 
+        award description, the committee stated: 
+        <blockquote>
+        "Darlene has been working with refugees, chairing the Human Rights Commission,
+         organizing on behalf of women, and doing everything possible to keep the City Council and the city itself progressive, 
+         openminded and justice-minded...She is doggedly committed to labor, reproductive choice, and has worked in Cambridge 
+         for both, organizing in support of UNITE workers, and educating the City Council on working conditions. Wherever justice 
+         is being made in the Cambridge area, you'll find one and usually both of these women. They work at the grassroots 
+         steadily, effectively and with grace and humor."
+       </blockquote>
+
+        Nicgorski became the chair of the city's Human Rights Commission and the couple marched with the Cambridge LGBT group 
+        during many Boston Gay Pride Marches. In 2001 the couple moved to North Carolina. In 2008, they were married in San 
+        Francisco, California, later relocating to southern California. Nicgorski received many awards in her lifetime, 
+        including the 1987 <i>Ms.</i> magazine Woman of the Year, the <i>Earl Warren Civil Liberties Award</i> from the
+         ACLU of Northern California, and the <i>Mayor's Proclamation and Key to the City</i> in Santa Cruz.
+         <br><Br>
+         Nicgorski died in Pomona, California, on February 28, 2017, at the age of 73.
+
+
+
+       
+            `,
+        otherNotes: "",
+        references: `
+         Peace Commission Awardees:<Br>
+         <a class='indent' href="https://www.cambridgema.gov/-/media/Files/peacecommission/peaceandjusticeawardees19952006.pdf">https://www.cambridgema.gov/-/media/Files/peacecommission/peaceandjusticeawardees19952006.pdf</a>
+         <br>  
+         University of Arizona's Sanctuary Movement Papers:<br>     
+        <a class='indent' href="https://lib.arizona.edu/special-collections/collections/sanctuary-movement-trial-papers">https://lib.arizona.edu/special-collections/collections/sanctuary-movement-trial-papers</a>
+        <br>
+        <i>The Cambridge Chronicle</i>, 10 October 1996, 15 January 1998<br>
+        <i>Claremont Courier</i> Obituary:<br>
+        <a class='indent' href="https://claremont-courier.com/obituaries/t22635-nicgorski-33107/">https://claremont-courier.com/obituaries/t22635-nicgorski-33107/</a>
+        <Br>
+        Correspondence with Chris Blackburn
+
+        `,
+        photos: ["images_bios/chrisanddar2.jpg", "images_bios/chrisanddar.jpg"],
+        photoTitles: [
+            "Chris (L) and Dar (R), North Cambridge, c.1992", "Chris (L) and Dar (R), at a 2017 demonstration in California"
+        ],
+        dateCreated: "05.08.2026",
+        dateUpdated: "",
+
+        photoHeights: [""],
+        editors: "Meta Partenheimer",
+        authors: "Anthony Beckwith",
+        categories: ["Human Rights Activist"],
+    },
     //     {
     //     poc: false,
     //     organization: false,
